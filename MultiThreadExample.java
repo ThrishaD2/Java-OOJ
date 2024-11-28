e80f4958922be784fb9ca1566fb2574c990be0c5
ThrishaD2 <thrishad02@gmail.com>
2024-11-28 06:37:53 +0530

    Create MultiThreadExample.java

---
+++ MultiThreadExample.java
@@ -0,0 +1,37 @@
+class BMSCollegeThread extends Thread {
+    public void run() {
+        try {
+            while (true) {
+                System.out.println("BMS College of Engineering");
+                Thread.sleep(10000); 
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Thread interrupted: " + e.getMessage());
+        }
+    }
+}
+
+class CSEThread extends Thread {
+    public void run() {
+        try {
+            while (true) {
+                System.out.println("CSE");
+                Thread.sleep(2000); 
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Thread interrupted: " + e.getMessage());
+        }
+    }
+}
+
+public class MultiThreadExample {
+    public static void main(String[] args) {
+        
+        BMSCollegeThread bmsThread = new BMSCollegeThread();
+        CSEThread cseThread = new CSEThread();
+        
+        
+        bmsThread.start();
+        cseThread.start();
+    }
+}
